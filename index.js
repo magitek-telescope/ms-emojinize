be415ec21754b6c0e4793b1d377586f96bad27e0
Takuma Hanatani <mail@potato4d.me>
2017-02-13 16:43:58 +0900

    Fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const emojis = require("./dictionary");
 const emojinize = (text) => {
-  return text.replace(/:([a-zA-Z0-9+-_]*?):/g, (match, part)=>{
+  return (text || "").replace(/:([a-zA-Z0-9+-_]*?):/g, (match, part)=>{
     const target = emojis.find((emoji)=>{
-      return part in emoji.aliases;
+      return emoji.aliases.indexOf(part) !== -1;
     });
     return target ? target.emoji : match;
   });
-}
+};
 
 module.exports = emojinize;
